2ea96b2c549f19affdcd88b684d32782a701cf00
Nick Marshall <nmarshall@highkeep.com>
2023-04-21 15:08:50 -0700

    added builder user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-pacman -Syu --noconfirm --needed git svn
+pacman -Syu --noconfirm --needed base git svn
+
+useradd builder -m
+# When installing dependencies, makepkg will use sudo
+# Give user `builder` passwordless sudo access
+echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+
+# Give all users (particularly builder) full access to these files
+chmod -R a+rw .
 
 if "${INPUT_SVN}"; then
-    git svn clone "${INPUT_REPOURL}" --trunk=trunk/${INPUT_REPOPKG} $(basename "${INPUT_REPOPKG}")
+    sudo -u builder git svn clone "${INPUT_REPOURL}" --trunk=trunk/${INPUT_REPOPKG} $(basename "${INPUT_REPOPKG}")
 else
-    git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
+    sudo -u builder git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
 fi
