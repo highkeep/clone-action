b72ac9d4ade863d08b74dc721f68ceac2704a8e7
Nick Marshall <nmarshall@highkeep.com>
2023-04-21 15:53:37 -0700

    issues were with tested package - cleaned in the process

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,8 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .
 
-echo "${INPUT_SVNTARGET}"
-
 if [ "${INPUT_SVNTARGET:-false}" == true ]; then
-    echo "svn=true"
-    #sudo -u builder git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
+    sudo -u builder git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
 else
-    echo "svn=false"
-    #sudo -u builder git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
+    sudo -u builder git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
 fi
