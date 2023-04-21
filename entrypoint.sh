c63d428671084bb205dade9c8aab7e1df79f6f12
Nick Marshall <nmarshall@highkeep.com>
2023-04-21 16:20:58 -0700

    testing scraping builder

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,25 @@ set -euo pipefail
 # Install required packages
 pacman -Syu --noconfirm --needed sudo git svn
 
-# Added builder as seen in edlanglois/pkgbuild-action
-# mainly for permissions
-useradd builder -m
-# When installing dependencies, makepkg will use sudo
-# Give user `builder` passwordless sudo access
-echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+# # Added builder as seen in edlanglois/pkgbuild-action
+# # mainly for permissions
+# useradd builder -m
+# # When installing dependencies, makepkg will use sudo
+# # Give user `builder` passwordless sudo access
+# echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 
-# Give all users (particularly builder) full access to these files
-chmod -R a+rw .
+# # Give all users (particularly builder) full access to these files
+# chmod -R a+rw .
 
 if [ "${INPUT_SVNTARGET:-false}" == true ]; then
-    sudo -u builder git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
+    # sudo -u builder git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
+    git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
     ref=$(git -C "${INPUT_REPOPKG##*/}" rev-parse HEAD)
     echo "pkg=${INPUT_REPOPKG##*/}" >>$GITHUB_OUTPUT
     echo "pkgRef=${ref}" >>$GITHUB_OUTPUT
 else
-    sudo -u builder git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
+    # sudo -u builder git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
+    git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
     ref=$(git -C "${INPUT_REPOPKG}" rev-parse HEAD)
     echo "pkg=${INPUT_REPOPKG}" >>$GITHUB_OUTPUT
     echo "pkgRef=${ref}" >>$GITHUB_OUTPUT
