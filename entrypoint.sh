7efc7cd6f88380288797db1db229338845287311
Nick Marshall <nmarshall@highkeep.com>
2023-04-22 17:33:54 -0700

    tidy; testing svn calls

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,19 @@ echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .
 
+# Set up sudo cmd to make life a little easier
+sudoCMD="sudo -H -u builder"
+
 if [ "${INPUT_SVNTARGET:-false}" == true ]; then
-    sudo -u builder git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
-    ref=$(sudo -u builder git -C "${INPUT_REPOPKG##*/}" rev-parse HEAD)
+    echo "Cloning: ${INPUT_REPOURL} into: ${INPUT_REPOPKG##*/}"
+    ${sudoCMD} git svn clone "${INPUT_REPOURL}" --trunk=trunk/"${INPUT_REPOPKG}" "${INPUT_REPOPKG##*/}"
+    ref=$(${sudoCMD} git -C "${INPUT_REPOPKG##*/}" rev-parse HEAD)
     echo "pkg=${INPUT_REPOPKG##*/}" >>$GITHUB_OUTPUT
     echo "pkgRef=${ref}" >>$GITHUB_OUTPUT
 else
-    sudo -u builder git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
-    ref=$(sudo -u builder git -C "${INPUT_REPOPKG}" rev-parse HEAD)
+    echo "Cloning: ${INPUT_REPOURL} into: ${INPUT_REPOPKG}"
+    ${sudoCMD} git clone "${INPUT_REPOURL}" "${INPUT_REPOPKG}"
+    ref=$(${sudoCMD} git -C "${INPUT_REPOPKG}" rev-parse HEAD)
     echo "pkg=${INPUT_REPOPKG}" >>$GITHUB_OUTPUT
     echo "pkgRef=${ref}" >>$GITHUB_OUTPUT
 fi
